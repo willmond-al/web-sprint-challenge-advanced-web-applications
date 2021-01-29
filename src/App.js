b4fca587f5368a4f1c92fac9329ec42c2e593f69
willmondal <wbmondal@aol.com>
2021-01-29 09:22:11 -0800

    PrivateRoute added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import PrivateRoute from './components/PrivateRoute'
 
 import Login from "./components/Login";
+import BubblePage from './components/BubblePage'
 import "./styles.scss";
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
+        <PrivateRoute exact path="/bubble-page" component={BubblePage}/>
       </div>
     </Router>
   );
--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,7 +1,21 @@
+import React, { Component } from 'react'
+import { Route, Redirect } from 'react-router-dom'
+// import { render } from 'node-sass'
 
 
+const PrivateRoute = ({ component: Component, ...rest }) =>{
+    return(<Route {...rest} render={
+        props =>{
+            if(localStorage.getItem("token")) {
+                return <Component {...props}/>
+            } else {
+                return <Redirect to='/'/>
+            }
+        }
+    }/>)
+}
 
-
+export default PrivateRoute
 
 
 
